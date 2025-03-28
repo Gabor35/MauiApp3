88951e14c54478691837048d58137c26545de151
Gabor35 <pusztaig@kkszki.hu>
2025-03-28 10:15:40 +0100

    Add project files.

---
+++ MauiApp3/Models/Esemeny.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp3.Models
+{
+    public partial class Esemeny
+    {
+        public int Id { get; set; }
+
+        public string Cime { get; set; } = null!;
+
+        public string Helyszin { get; set; } = null!;
+
+        public DateTime Datum { get; set; }
+
+        public string? Leiras { get; set; }
+
+        public string Kepurl { get; set; } = null!;
+
+    }
+
+}
---
+++ MauiApp3/Services/ApiService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using MauiApp3.Models;
+
+namespace MauiApp3.Services
+{
+    public class ApiService
+    {
+        private readonly HttpClient _httpClient;
+
+        public ApiService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<List<Esemeny>> GetEsemenyekAsync()
+        {
+            var response = await _httpClient.GetAsync("esemeny");
+            if (!response.IsSuccessStatusCode)
+            {
+                var error = await response.Content.ReadAsStringAsync();
+                throw new Exception($"Hiba történt: {response.StatusCode} - {error}");
+            }
+
+            var json = await response.Content.ReadAsStringAsync();
+            var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+            return JsonSerializer.Deserialize<List<Esemeny>>(json, options);
+        }
+    }
+
+    
+    }
